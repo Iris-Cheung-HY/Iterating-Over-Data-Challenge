6b1718442f973e8cd7850da0dff8d0a936bcb6b0
Iris Cheung <hokyinirischeung@gmail.com>
2025-09-11 12:26:09 -0700

    completed 1 and 2

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 def calculate_total_sales(sales_totals):
-    pass
+    total = 0
+    for candy, value in sales_totals.items():
+        q_price = value["quantity"] * value["price"]
+        total += q_price
+    return total
 
 def calculate_average_sales(sales_totals):
-    pass
+    total = 0
+    for candy, value in sales_totals.items():
+        q_price = value["quantity"] * value["price"]
+        total += q_price
+    if sales_totals == {}:
+        average = 0.0
+    else:
+        average = total/len(sales_totals)
+    return average 
+    
 
 def filter_to_better_than_average_sales(sales_totals):
-    pass
+    filtered = {}
+    total = 0
+    for candy, value in sales_totals.items():
+        q_price = value["quantity"] * value["price"]
+        total += q_price
+    if sales_totals == {}:
+        average = 0.0
+    else:
+        average = total/len(sales_totals)
+    
+    if q_price > average:
+        filtered.append(candy)
+        
+
+
     
 def ninety_nine_bottles(count, beverage):
     pass
